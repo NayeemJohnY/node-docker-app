0cefdcd32b5d92a8113b114c314048e61170fdd0
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2022-09-04 17:24:03 +0530

    Add files via upload

--- api-app/index.js
+++ api-app/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const morgan = require('morgan')
+const { logger} = require('./logger')
 
 const app = express();
 
+app.use(morgan(':remote-addr :url :method HTTP/:http-version :user-agent', {
+    immediate: true,
+    stream: {
+        write: (message) => {
+            logger.info('APIENTRY', message);
+        }
+    }
+}));
+app.use(morgan(":remote-addr :url :method  :req[Content-length] :status :response-time ms", {
+    stream: {
+        write: (message) => {
+            logger.info('APIEXIT', message);
+        }
+    }
+}));
 
 app.get('/health', (req, res) => {
     res.status(200).send(`<h2> I am in working condition. Thanks for checking <h2>`)
@@ -11,7 +28,7 @@ app.get('/about', (req, res) => {
     res.status(200).send(`<h2> About this container group instance <h2>`)
 })
 
-PORT = process.env.PORT | 8080
+PORT = process.env.PORT || 8080
 app.listen(PORT, ()=> {
     console.log(`Running on PORT ${PORT}`);
 })
