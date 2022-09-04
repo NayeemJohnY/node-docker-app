0a3f68bac2b2888895f3dedab0543ba78ff42e4c
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2022-09-04 13:40:22 +0530

    another app

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the HTML Node JS app</h1>')
+app.get('/health', (req, res) => {
+    res.status(200).send(`<h2> I am in working condition. Thanks for checking <h2>`)
 })
 
-PORT = process.env.PORT |8080
+app.get('/about', (req, res) => {
+    res.status(200).send(`<h2> About this container group instance <h2>`)
+})
+
+PORT = process.env.PORT | 8080
 app.listen(PORT, ()=> {
     console.log(`Running on PORT ${PORT}`);
 })
