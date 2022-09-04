12fc25b9f7babcc961eaffeb8f564c4da87b8a23
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2022-09-04 13:44:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/health', (req, res) => {
-    res.status(200).send(`<h2> I am in working condition. Thanks for checking <h2>`)
-})
-
-app.get('/about', (req, res) => {
-    res.status(200).send(`<h2> About this container group instance <h2>`)
-})
 
 app.get('/', (req, res) => {
     res.send(`
